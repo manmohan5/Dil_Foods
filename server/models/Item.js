161264aadb07aa1c9f58f372f838bd32da5296a4
manmohan5 <manmohancs27@gmail.com>
2023-04-11 19:11:33 +0530

    add name in order

--- server/models/Item.js
+++ server/models/Item.js
@@ -4,6 +4,7 @@ const { Schema, model } = mongoose;
 const itemSchema = new Schema({
   price: { type: Number, unique: false },
   quantity: { type: Number, unique: false },
+  name: { type: String, unique: false },
   date: { type: Date, default: Date.now },
   order: {
     type: mongoose.Schema.Types.ObjectId,
